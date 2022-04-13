343c85cbf47340ef0cc38fbee1d8555c12258ec9
kalpshah485 <kalpshah485@gmail.com>
2022-04-13 15:48:23 +0530

    feat:Added graphql queries and mutation.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { gql, useMutation, useQuery } from '@apollo/client';
+
+// const EXCHANGE_RATES = gql`
+//   query GetExchangeRates {
+//     rates(currency: "INR") {
+//       currency
+//       rate
+//     }
+//   }
+// `;
+const GET_USERS = gql`
+  query getUsers {
+    users {
+      id
+      name
+      rocket
+    }
+  }
+`;
+const INSERT_USER = gql`
+  mutation insertUser($name: String!,$rocket: String!) {
+    insert_users(objects: { name: $name, rocket: $rocket }) {
+      returning {
+        id
+        name
+        rocket
+        timestamp
+      }
+    }
+  }
+`;
 
 function App() {
+  // const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  const { data, loading, error, refetch } = useQuery(GET_USERS);
+  const [name, setName] = useState("");
+  const [rocket, setRocket] = useState("");
+  console.log(data);
+  const [insertUser] = useMutation(INSERT_USER);
+  const addUser = () => {
+    insertUser({
+      variables: {
+        name: name,
+        rocket: rocket
+      },
+      onCompleted: (data) => {
+        refetch();
+        console.log(data)
+      }
+    }
+    );
+  }
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>Error...</p>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Apollo Client Mutation Tutorial</h1>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <input type="text" value={rocket} onChange={(e) => setRocket(e.target.value)} />
+      <button onClick={addUser}>
+        Add User
+      </button>
+      {
+        data.users.map((user) => {
+          return (
+            <div key={user.id}>
+              {user.name} {user.rocket}
+            </div>
+          )
+        })
+      }
+      {/* <h1>Apollo Client Tutorial</h1>
+      <div>
+        {
+          data.rates.map(({ currency, rate }) => (
+            <div key={currency}>
+              <p>
+                {currency}: {rate}
+              </p>
+            </div>
+          ))
+        }
+      </div> */}
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+  uri: 'https://api.spacex.land/graphql/',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client} >
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
